bddbd0a96856866fe4a116f8d3c7f73fed8be3a9
mgrover1 <maxi2312@gmail.com>
2022-07-26 13:18:30 -0500

    add lrose instalation

---
+++ install/lrose/build_lrose.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# show commands before execution
+set -x
+
+# do not fail GHA on nonzero exit status
+set +e
+
+# download the latest installation of lrose
+wget https://github.com/NCAR/lrose-core/releases/download/lrose-core-20220222/lrose-core-20220222.ubuntu_20.04.amd64.deb
+
+# install using apt-get
+apt-get install -y ./lrose-core-20190129.debian_9.amd64.deb
