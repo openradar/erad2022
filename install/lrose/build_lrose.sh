f4bef0c22cfaf128ed74cacd6afe9e615c653be8
mgrover1 <maxi2312@gmail.com>
2022-07-26 13:38:12 -0500

    make sure in proper dir

--- install/lrose/build_lrose.sh
+++ install/lrose/build_lrose.sh
@@ -5,6 +5,9 @@ set -x
 # do not fail GHA on nonzero exit status
 set +e
 
+# move into the root directory for this repo
+cd $BALTRAD_INSTALL_ROOT
+
 # download the latest installation of lrose
 wget https://github.com/NCAR/lrose-core/releases/download/lrose-core-20220222/lrose-core-20220222.ubuntu_20.04.amd64.deb
 
